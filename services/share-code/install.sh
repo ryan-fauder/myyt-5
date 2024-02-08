88b1a57fb8c3c4f67edbee580e5b92851344e9b8
ryan-maps <ryan.auder@maps.com.br>
2024-02-08 20:54:19 -0300

    adding messages for services about replication

--- services/share-code/install.sh
+++ services/share-code/install.sh
@@ -8,24 +8,28 @@ cd sistemas_distribuidos
 
 if ! [ -d myyt-3 ]
 then
+    echo "Iniciando o clone do repo"
     git clone https://github.com/ryan-maps/myyt-3.git
     cd ./myyt-3/datanode-server/
     rm ./alias.txt
 else
+    echo "Atualizando o repo"
     cd ./myyt-3/datanode-server/
     git pull
     rm ./alias.txt
 fi
-
+echo "Instalando dependencias"
 pip install rpyc, alembic, flask, sqlalchemy
 
 if ! [ -d database ]
 then
     mkdir database
+    echo "Instalando o banco"
     alembic upgrade head
 else
     rm -rf ./database
     mkdir database
+    echo "Instalando o banco"
     alembic upgrade head
 fi
 
